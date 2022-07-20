18b8a5a70da5434fe86aa41585b39c9a5f14692b
Ayush Gupta <721ayush@gmail.com>
2022-07-20 16:02:14 +0530

    Create peakIndex.cpp

---
+++ Array/peakIndex.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size() -1;
+        int leftSum = 0;
+        int sum = 0;
+        for(int i= 0;i<nums.size();i++){
+            sum += nums[i];
+        }
+        for(int i =  0;i<nums.size();i++){
+            sum -= nums[i];
+            
+            if(leftSum == sum)
+                return i;
+            leftSum += nums[i];
+                
+        }
+        return -1;
+    }
+};
